6837caee416f7538b8f9b5495bb1809044e93a22
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-16 14:34:24 -0300

    feat: finaliza documentacao com swagger no RegistroCancelaDTO

--- projeto-cancela/src/main/java/com/weg/projeto_cancela/infra/web/dto/RegistroCancelaDTO.java
+++ projeto-cancela/src/main/java/com/weg/projeto_cancela/infra/web/dto/RegistroCancelaDTO.java
@@ -1,8 +1,16 @@
 package com.weg.projeto_cancela.infra.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Objeto de transferência de dados representando um Registro na Cancela")
 public record RegistroCancelaDTO(
+        @Schema(description = "ID único gerado pelo banco")
         String id,
+
+        @Schema(description = "Tipo de evento que ocorreu")
         String evento,
+
+        @Schema(description = "LocalDate representando o horario em que foi feito o Registro")
         String data
 ) {
 }
