93e4fc9c69a5eb1df84cb5f22505658eef07239d
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2026-03-16 14:34:11 -0300

    feat: finaliza documentacao com swagger na SaidasTurnoController

--- projeto-cancela/src/main/java/com/weg/projeto_cancela/infra/web/controller/turno/SaidasTurnoController.java
+++ projeto-cancela/src/main/java/com/weg/projeto_cancela/infra/web/controller/turno/SaidasTurnoController.java
@@ -3,6 +3,10 @@ package com.weg.projeto_cancela.infra.web.controller.turno;
 import com.weg.projeto_cancela.application.service.SaidasService;
 import com.weg.projeto_cancela.domain.model.RegistroCancela;
 import com.weg.projeto_cancela.application.service.EntradasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/turno/saidas")
 @CrossOrigin(origins = "*")
+@Tag(name = "Saidas-Turno")
 public class SaidasTurnoController {
     private SaidasService service;
 
@@ -20,16 +25,31 @@ public class SaidasTurnoController {
         this.service = service;
     }
 
+    @Operation(summary = "Busca todas as saidas no primeiro turno de hoje", description = "Retorna o total de saidas no primeiro turno de hoje")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Total de saidas no primeiro turno de hoje não encontrado")
+    })
     @GetMapping("/primeiro")
     public List<RegistroCancela> getEntradaPrimeiroTurno(){
         return service.buscarSaidasTurno(1);
     }
 
+    @Operation(summary = "Busca todas as saidas no segundo turno de hoje", description = "Retorna o total de saidas no segundo turno de hoje")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Total de saidas no segundo turno de hoje não encontrado")
+    })
     @GetMapping("/segundo")
     public List<RegistroCancela> getEntradaSegundoTurno(){
         return service.buscarSaidasTurno(2);
     }
 
+    @Operation(summary = "Busca todas as saidas no terceiro turno de hoje", description = "Retorna o total de saidas no terceiro turno de hoje")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Total de saidas no terceiro turno de hoje não encontrado")
+    })
     @GetMapping("/terceiro")
     public List<RegistroCancela> getEntradaTerceiroTurno(){
         return service.buscarSaidasTurno(3);
